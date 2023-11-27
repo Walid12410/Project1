dac0bdb41bba6285a65ecbac2189551f4d768f2f
Haydar-Script <124156886+Haydar-Script@users.noreply.github.com>
2023-11-27 12:21:42 +0200

    Add files via upload

---
+++ lib/Temperture/Temp_constructor.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Temp{
+  double celsiusToFahrenheit(double celsius) {
+    return (celsius * 9 / 5) + 32;
+  }
+
+  double fahrenheitToCelsius(double fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+  }
+
+  double celsiusToKelvin(double celsius) {
+    return celsius + 273.15;
+  }
+
+  double kelvinToCelsius(double kelvin) {
+    return kelvin - 273.15;
+  }
+
+  double fahrenheitToKelvin(double fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9 + 273.15;
+  }
+
+  double kelvinToFahrenheit(double kelvin) {
+    return kelvin * 9 / 5 - 459.67;
+  }
+
+}
